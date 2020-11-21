22af49d615bdcfed5e7b104f5e66f3839096672a
zhangjianyun <zhangjianyun_kd@163.com>
2020-11-21 12:51:37 +0800

    整合zipkin和sleuth

--- src/main/java/com/lengedyun/gateway/controller/TestController.java
+++ src/main/java/com/lengedyun/gateway/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.lengedyun.gateway.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("gateway-test")
+@RefreshScope //nacos管理的配置属性动态刷新
 public class TestController {
 
     @Value("${your.config}")
